a71d30003c0739ade9359f226fc30eb4ced81325
overllord <71321510+Overllord2017@users.noreply.github.com>
2022-07-23 16:14:12 +0800

    Update products_db.sql

--- 5. internet_magazin/products_db.sql
+++ 5. internet_magazin/products_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Июл 23 2022 г., 07:04
+-- Время создания: Июл 23 2022 г., 11:13
 -- Версия сервера: 8.0.24
 -- Версия PHP: 7.4.21
 
@@ -20,25 +20,6 @@ SET time_zone = "+00:00";
 --
 -- База данных: `products_db`
 --
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `extra_images`
---
-
-CREATE TABLE `extra_images` (
-  `id` int NOT NULL,
-  `extra_images` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
---
--- Дамп данных таблицы `extra_images`
---
-
-INSERT INTO `extra_images` (`id`, `extra_images`) VALUES
-(1, 'images_11.png'),
-(2, 'images_22.png');
 
 -- --------------------------------------------------------
 
@@ -69,10 +50,10 @@ INSERT INTO `images` (`id_images`, `image`, `alt`) VALUES
 CREATE TABLE `products` (
   `id_products` int NOT NULL,
   `name` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `price` decimal(20,2) NOT NULL,
+  `price_without_discount` decimal(20,2) NOT NULL,
+  `promocode_price` decimal(20,2) NOT NULL,
   `description` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `price` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `price_without_discount` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `promocode_price` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `count_products` int DEFAULT NULL,
   `articul` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
@@ -81,11 +62,30 @@ CREATE TABLE `products` (
 -- Дамп данных таблицы `products`
 --
 
-INSERT INTO `products` (`id_products`, `name`, `description`, `price`, `price_without_discount`, `promocode_price`, `count_products`, `articul`) VALUES
-(1, 'РУБАШКА MEDICINE', 'Рубашка Medicine выполнена из вискозной ткани с клетчатым узором. Детали: прямой крой; отложной воротник; планка из маджеты на пуговицах; карман на груди.', '2499.00', '2699.00', '2227.00', 5, 1),
-(2, 'Худи Medicine Худи', 'Толстовка удлинённая и прежде всего привлекает своей длиной по спинке (до 78 см), объёмными рукавами и манжетом с прорезью для большого пальца. ', '2900.00', '3100.00', '2600.00', 21, 2),
-(3, 'Худи COMFYCLTH', 'Худи объемное с накладным карманом и двойным капюшоном. Комфортный оверсайз со спущенным плечом. Начёс в процессе носки может скатываться. Универсальный крой позволяет сочетать и носить данное изделие с абсолютно любыми брюками', '3500.00', '3850.00', '3400.00', 10, 2),
-(4, 'Шорты спортивные Top Top', 'Данный товар является частью проекта Lamoda planet - специального раздела нашего каталога, где мы собрали экологичные, этичные, инклюзивные и благотворительные товары.', '4200.00', '4500.00', '3900.00', NULL, 2);
+INSERT INTO `products` (`id_products`, `name`, `price`, `price_without_discount`, `promocode_price`, `description`, `count_products`, `articul`) VALUES
+(1, 'РУБАШКА MEDICINE', '2600.00', '2800.00', '2500.00', 'Рубашка Medicine выполнена из вискозной ткани с клетчатым узором. Детали: прямой крой; отложной воротник; планка из маджеты на пуговицах; карман на груди.', 5, 1),
+(2, 'Худи Medicine Худи', '3500.00', '4000.00', '3400.00', 'Толстовка удлинённая и прежде всего привлекает своей длиной по спинке (до 78 см), объёмными рукавами и манжетом с прорезью для большого пальца. ', 21, 2),
+(3, 'Худи COMFYCLTH', '3400.00', '3750.00', '2000.00', 'Худи объемное с накладным карманом и двойным капюшоном. Комфортный оверсайз со спущенным плечом. Начёс в процессе носки может скатываться. Универсальный крой позволяет сочетать и носить данное изделие с абсолютно любыми брюками', 10, 2),
+(4, 'Шорты спортивные Top Top', '2500.00', '2800.00', '2300.00', 'Данный товар является частью проекта Lamoda planet - специального раздела нашего каталога, где мы собрали экологичные, этичные, инклюзивные и благотворительные товары.', NULL, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `product_extra_picthers`
+--
+
+CREATE TABLE `product_extra_picthers` (
+  `id_product` int DEFAULT NULL,
+  `id_images` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Дамп данных таблицы `product_extra_picthers`
+--
+
+INSERT INTO `product_extra_picthers` (`id_product`, `id_images`) VALUES
+(1, 1),
+(1, 2);
 
 -- --------------------------------------------------------
 
@@ -114,7 +114,6 @@ INSERT INTO `product_pictures` (`products_id_products`, `images_id_images`) VALU
 
 CREATE TABLE `sections` (
   `id_sections` int NOT NULL,
-  `main_section_id` int DEFAULT NULL,
   `name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `description` text CHARACTER SET utf8 COLLATE utf8_general_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
@@ -123,11 +122,11 @@ CREATE TABLE `sections` (
 -- Дамп данных таблицы `sections`
 --
 
-INSERT INTO `sections` (`id_sections`, `main_section_id`, `name`, `description`) VALUES
-(1, NULL, 'Летняя одежда', 'Яркая одежда на лето, рубашки, футболки, шорты и тд.'),
-(2, NULL, 'Рубашки', 'Рубашки буржуйских и отечественных брендов'),
-(3, 1, 'Худи', 'Очень теплые худи, мягкие и очень удобные.'),
-(4, NULL, 'Зимняя одежда', 'Теплая одежда для холодной зимы');
+INSERT INTO `sections` (`id_sections`, `name`, `description`) VALUES
+(1, 'Летняя одежда', 'Яркая одежда на лето, рубашки, футболки, шорты и тд.'),
+(2, 'Рубашки', 'Рубашки буржуйских и отечественных брендов'),
+(3, 'Худи', 'Очень теплые худи, мягкие и очень удобные.'),
+(4, 'Зимняя одежда', 'Теплая одежда для холодной зимы');
 
 -- --------------------------------------------------------
 
@@ -137,28 +136,23 @@ INSERT INTO `sections` (`id_sections`, `main_section_id`, `name`, `description`)
 
 CREATE TABLE `sections_has_products` (
   `sections_id_sections` int NOT NULL,
-  `products_id_products` int NOT NULL
+  `products_id_products` int NOT NULL,
+  `is_main` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Дамп данных таблицы `sections_has_products`
 --
 
-INSERT INTO `sections_has_products` (`sections_id_sections`, `products_id_products`) VALUES
-(2, 1),
-(3, 2),
-(3, 3),
-(1, 4);
+INSERT INTO `sections_has_products` (`sections_id_sections`, `products_id_products`, `is_main`) VALUES
+(1, 4, 1),
+(2, 1, 0),
+(3, 2, 0),
+(3, 3, 0);
 
 --
 -- Индексы сохранённых таблиц
 --
-
---
--- Индексы таблицы `extra_images`
---
-ALTER TABLE `extra_images`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `images`
@@ -173,6 +167,13 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`id_products`);
 
 --
+-- Индексы таблицы `product_extra_picthers`
+--
+ALTER TABLE `product_extra_picthers`
+  ADD KEY `id_product` (`id_product`),
+  ADD KEY `id_images` (`id_images`);
+
+--
 -- Индексы таблицы `product_pictures`
 --
 ALTER TABLE `product_pictures`
@@ -185,8 +186,7 @@ ALTER TABLE `product_pictures`
 --
 ALTER TABLE `sections`
   ADD PRIMARY KEY (`id_sections`),
-  ADD UNIQUE KEY `name` (`name`),
-  ADD UNIQUE KEY `main_section_id` (`main_section_id`) USING BTREE;
+  ADD UNIQUE KEY `name` (`name`);
 
 --
 -- Индексы таблицы `sections_has_products`
@@ -199,12 +199,6 @@ ALTER TABLE `sections_has_products`
 --
 -- AUTO_INCREMENT для сохранённых таблиц
 --
-
---
--- AUTO_INCREMENT для таблицы `extra_images`
---
-ALTER TABLE `extra_images`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT для таблицы `images`
@@ -229,10 +223,11 @@ ALTER TABLE `sections`
 --
 
 --
--- Ограничения внешнего ключа таблицы `extra_images`
+-- Ограничения внешнего ключа таблицы `product_extra_picthers`
 --
-ALTER TABLE `extra_images`
-  ADD CONSTRAINT `extra_images_ibfk_1` FOREIGN KEY (`id`) REFERENCES `products` (`id_products`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `product_extra_picthers`
+  ADD CONSTRAINT `product_extra_picthers_ibfk_1` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_products`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `product_extra_picthers_ibfk_2` FOREIGN KEY (`id_images`) REFERENCES `images` (`id_images`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Ограничения внешнего ключа таблицы `product_pictures`
@@ -240,12 +235,6 @@ ALTER TABLE `extra_images`
 ALTER TABLE `product_pictures`
   ADD CONSTRAINT `fk_products_has_images_images1` FOREIGN KEY (`images_id_images`) REFERENCES `images` (`id_images`),
   ADD CONSTRAINT `fk_products_has_images_products1` FOREIGN KEY (`products_id_products`) REFERENCES `products` (`id_products`);
-
---
--- Ограничения внешнего ключа таблицы `sections`
---
-ALTER TABLE `sections`
-  ADD CONSTRAINT `section__parent__fk` FOREIGN KEY (`main_section_id`) REFERENCES `sections` (`id_sections`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Ограничения внешнего ключа таблицы `sections_has_products`
@@ -258,6 +247,8 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
 /*
 1.
 SELECT s.id_sections,
